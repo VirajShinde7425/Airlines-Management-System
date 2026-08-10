82fb1925492ccc282be6787a3c1035fe690e49bb
VirajShinde7425 <vs922265@gmail.com>
2026-08-10 23:44:54 +0530

    setup spring boot backend with MySQL

---
+++ backend/src/main/java/Airline_Management_System/AirlineManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package Airline_Management_System;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AirlineManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AirlineManagementSystemApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/Airline_Management_System/AirlineManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package Airline_Management_System;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AirlineManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
